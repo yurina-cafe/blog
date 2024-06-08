357c4fc8f37835125b692bd074aedf1b70b11023
nesb01t <nesb01t@163.com>
2024-06-08 08:23:43 -0700

    update: init

---
+++ components/article-poster/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+export interface ArticlePosting {
+  title: string; // 文章的标题，会转化成文件名的第一部分
+  time: string; // 文章的时间，会转化成文件名的第二部分
+  tag: string; // 文章的标签，会转化成文件名的第三部分
+  content: string;
+}
+
+const article = ref<ArticlePosting>({
+  title: "",
+  time: "",
+  tag: "",
+  content: "",
+});
+
+const post = () => {
+  const data = {
+    title: `${article.value.title}@${Date.now()}@${article.value.tag}`,
+    content: article.value.content,
+  };
+  debugPostArticle(data);
+};
+</script>
+
+<template>
+  <div>
+    <div class="article-poster">
+      <h1>文章发布</h1>
+      <div>
+        <input type="text" v-model="article.title" placeholder="文章标题" />
+      </div>
+      <div>
+        <input type="text" v-model="article.tag" placeholder="文章标签" />
+      </div>
+      <div>
+        <textarea v-model="article.content" placeholder="文章内容"></textarea>
+      </div>
+      <div>
+        <button @click="post">发布</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="navigator">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/articles">Articles</NuxtLink>
+      <NuxtLink to="/post">Post</NuxtLink>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.navigator {
+  display: flex;
+  gap: 10px;
+}
+</style>
