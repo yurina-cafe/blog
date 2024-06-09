7b28f344675682a21dc5af1020b7a43229bcde03
chenmh0901 <1210204146@zust.edu.cn>
2024-06-09 02:27:18 -0400

    update poster layout

--- components/article-poster/index.vue
+++ components/article-poster/index.vue
@@ -23,16 +23,16 @@ const post = () => {
 </script>
 
 <template>
-  <div>
-    <div class="article-poster">
-      <h1>文章发布</h1>
-      <div>
+  <div class="article-poster">
+    <h1>文章发布</h1>
+    <div class="article">
+      <div class="text-input">
         <input type="text" v-model="article.title" placeholder="文章标题" />
       </div>
-      <div>
+      <div class="text-input">
         <input type="text" v-model="article.tag" placeholder="文章标签" />
       </div>
-      <div>
+      <div class="text-content">
         <textarea v-model="article.content" placeholder="文章内容"></textarea>
       </div>
       <div>
@@ -42,4 +42,39 @@ const post = () => {
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.article-poster {
+  height: 500px;
+  width: 500px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.article {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+}
+
+.text-input {
+  width: 400px;
+}
+
+.text-input input {
+  width: 100%;
+}
+
+.text-content {
+  width: 400px;
+}
+
+.text-content textarea {
+  width: 100%;
+  min-height: 200px;
+  resize: vertical;
+}
+</style>
