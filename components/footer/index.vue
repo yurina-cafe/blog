6e41d3418fbcff6f2ce99d2cd877be02a6f83f04
chenmh0901 <1210204146@zust.edu.cn>
2024-06-10 03:18:23 -0400

    update layout footer

---
+++ components/footer/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="footer">
+    amumu blog
+  </div>
+</template>
+
+<style coped>
+.footer {
+  height: 80px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #aaa;
+  background: repeating-linear-gradient(45deg,
+      #222,
+      #222 10px,
+      #111 10px,
+      #111 20px);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,40 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="layout">
     <div class="navigator">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/articles">Articles</NuxtLink>
       <NuxtLink to="/post">Post</NuxtLink>
     </div>
-    <slot />
+    <main class="main">
+      <slot />
+    </main>
+    <Footer />
   </div>
 </template>
 
 <style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+}
+
 .navigator {
   display: flex;
   gap: 10px;
+}
+
+.main {
+  max-width: 1200px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  margin: auto 0;
+  padding: 20px;
+  width: 100%;
 }
 </style>
