1625ea794fdcb78d01bed2af4090c8b51992723f
chenmh0901 <1210204146@zust.edu.cn>
2024-06-12 10:18:02 -0400

    update http

--- utils/http.ts
+++ utils/http.ts
@@ -1,16 +1,13 @@
-import type { AxiosRequestConfig } from "axios"
-import { service } from "./service"
-
-export const http = {
-  get<T = any>(url: string, config?: AxiosRequestConfig): Promise<T> {
-    return service.get(url, config)
-  },
-
-  post<T = any>(url: string, data?: object, config?: AxiosRequestConfig): Promise<T> {
-    return service.post(url, data, config)
-  },
-
-  delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<T> {
-    return service.delete(url, config)
-  }
-}
+import axios from "axios";
+export const useHttp = (
+  path: string,
+  method: "GET" | "POST" | "PUT" | "DELETE" = "GET",
+  data?: any
+) => {
+  const config = useRuntimeConfig()
+  return axios({
+    method,
+    url: config.public.baseApi + path,
+    data
+  });
+};
