99cff45a2f71e831299e0e18b9c23478145a409b
amumu <1210204146@zust.edu.cn>
2024-08-05 11:33:54 +0800

    update navigator

--- components/navigator/index.vue
+++ components/navigator/index.vue
@@ -1,29 +1,38 @@
 <script setup lang="ts">
+import { Icon } from '@iconify/vue';
 
+const pageSets = [
+  {
+    href: '/articles',
+    title: '笔记',
+    icon: 'material-symbols:folder-rounded',
+  },
+  {
+    href: '/post',
+    title: '上传',
+    icon: 'material-symbols:drive-folder-upload-rounded',
+  }
+]
 </script>
 
 <template>
   <div class="navigator">
-    <div class="navigator-content">
+    <div class="navigator__content">
       <div class="flex-1">
         <NuxtLink to="/">
-          <button class="btn btn-ghost navigator__title">
-            Home
+          <button class="btn btn-ghost">
+            <Icon icon="material-symbols:book-2-rounded" height="24px" />
+            <p class="navigator__title">博客 Amumu</p>
+            <p class="navigator__subtitle">一些作品和笔记</p>
           </button>
         </NuxtLink>
       </div>
       <div class="flex gap-2">
-        <div>
-          <NuxtLink to="/articles">
+        <div v-for="page in pageSets" :key="page.title">
+          <NuxtLink :to="page.href">
             <button class="btn btn-ghost">
-              Articles
-            </button>
-          </NuxtLink>
-        </div>
-        <div>
-          <NuxtLink to="/post">
-            <button class="btn btn-ghost">
-              Post
+              <Icon :icon="page.icon" height="20px" />
+              <p>{{ page.title }}</p>
             </button>
           </NuxtLink>
         </div>
@@ -37,14 +46,25 @@
   @apply h-[50px] w-full;
   @apply flex justify-center my-2;
 
-  &-content {
-    @apply backdrop-blur max-w-[1200px] w-full;
+  &__content {
+    @apply backdrop-blur max-w-[1200px] w-full px-[20px];
     @apply flex items-center;
   }
 
   &__title {
     font-size: 18px;
-    font-weight: 700;
   }
+
+  &__subtitle {
+    font-size: 16px;
+    @apply text-primary font-light;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .navigator__subtitle {
+    display: none;
+  }
+
 }
 </style>
